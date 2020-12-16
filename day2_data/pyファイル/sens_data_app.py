acf580ab6f090775af4344b6edd86710a4b47a97
Kimura Taiki <60126875+pokurin123@users.noreply.github.com>
2020-12-16 09:31:46 +0900

    Update sens_data_app.py

--- day2_data/pyファイル/sens_data_app.py
+++ day2_data/pyファイル/sens_data_app.py
@@ -1,9 +1,15 @@
-from dataaccess import DataAccess
+from pre_dataaccess import DataAccess
 import itertools
 
-hoge = DataAccess()
-count = 0
-x = list(itertools.chain.from_iterable(hoge.get_spots()))
-for i in ["spot_type:","rating_5Lv:","history:","culture:","nature:","religion:","amusement:"]:
-    print(i,x[count])
-    count += 1
+for id_n in list(range(1,10)):
+    hoge = DataAccess()
+    x = list(itertools.chain.from_iterable(hoge.get_spots(id_n)))
+    x.insert(6,"amusement")
+    x.insert(5,"religion")
+    x.insert(4,"nature")
+    x.insert(3,"culture")
+    x.insert(2,"history")
+    x.insert(1,"rating_5Lv")
+    x.insert(0,"spot_type")
+
+    print(x)
