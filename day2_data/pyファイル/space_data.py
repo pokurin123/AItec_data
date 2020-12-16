0eca2e109f374c20a56357ad735164c1f65c7f31
Kimura Taiki <60126875+pokurin123@users.noreply.github.com>
2020-12-16 09:06:08 +0900

    Update space_data.py

--- day2_data/pyファイル/space_data.py
+++ day2_data/pyファイル/space_data.py
@@ -1,8 +1,8 @@
 from var import Var
 from db import DB
 class DataAccess:
-    def get_spots(self):
-        query = "SELECT spot_location,spot_latitude,spot_longitude,nearest_station FROM my_spot4 WHERE ID = 1"
+    def get_spots(self,id_n):
+        query = "SELECT spot_location,spot_latitude,spot_longitude,nearest_station FROM my_spot4 WHERE ID =" + str(id_n)
         data = ()
         db = DB(Var.hostname, Var.port, Var.dbname, Var.username, Var.password)
         return db.execute(query, data)
