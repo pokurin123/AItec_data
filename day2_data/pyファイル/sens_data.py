876321ea250fc7fa0a90235b612fea880614e69a
Kimura Taiki <60126875+pokurin123@users.noreply.github.com>
2020-12-16 09:31:26 +0900

    Update sens_data.py

--- day2_data/pyファイル/sens_data.py
+++ day2_data/pyファイル/sens_data.py
@@ -1,8 +1,8 @@
 from var import Var
 from db import DB
 class DataAccess:
-    def get_spots(self):
-        query = "SELECT spot_type,rating_5Lv,history,culture,nature,religion,amusement FROM my_spot4 WHERE ID = 10"
+    def get_spots(self,id_n):
+        query = "SELECT spot_type,rating_5Lv,history,culture,nature,religion,amusement FROM my_spot4 WHERE ID =" + str(id_n)
         data = ()
         db = DB(Var.hostname, Var.port, Var.dbname, Var.username, Var.password)
         return db.execute(query, data)
