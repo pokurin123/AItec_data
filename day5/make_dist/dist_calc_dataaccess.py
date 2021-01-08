6ef3807035c6e6e405aec7ae7ed089d84ec7261e
Kimura Taiki <60126875+pokurin123@users.noreply.github.com>
2021-01-08 17:48:45 +0900

    Create dist_calc_dataaccess.py

---
+++ day5/make_dist/dist_calc_dataaccess.py
@@ -0,0 +1,8 @@
+from var import Var
+from db import DB
+class DataAccess:
+    def get_spots(self,id_n):
+        query = "SELECT spot_name,spot_location,spot_latitude,spot_longitude,nearest_station FROM my_spot4 WHERE ID =" + str(id_n)
+        data = ()
+        db = DB(Var.hostname, Var.port, Var.dbname, Var.username, Var.password)
+        return db.execute(query, data)
